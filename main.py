4b9cc6607c9cb13010cc2e02084a5290301f0509
nick <belyaev.frog01@gmail.com>
2024-11-17 09:50:34 +0300

    add proj

--- main.py
+++ main.py
@@ -1,61 +1,87 @@
 import requests
 import os
 import yaml
-import re
 import browser_cookie3
+import argparse
+import zipfile
+import shutil
 
-def download_zip(url, cookies):
-    # Добавляем '/download/zip' к URL
-    full_url = f"{url}/download/zip"
+def process_report(link, path, cookies):
+    dwnld_link = f"{link}/download/zip"
     
     try:
-        # Выполняем GET-запрос с куками
-        response = requests.get(full_url, cookies=cookies)
-        response.raise_for_status()  # Проверяем на наличие ошибок
+        response = requests.get(dwnld_link, cookies=cookies)
+        response.raise_for_status() 
         
-        # Сохраняем содержимое в файл
-        filename = "downloaded_file.zip"
+        filename = "report.zip"
         with open(filename, 'wb') as f:
             f.write(response.content)
         
-        print(f"Файл успешно загружен и сохранен как {filename}")
     except requests.exceptions.RequestException as e:
-        print(f"Ошибка при загрузке файла: {e}")
+        print(f"unable to download: {e}")
+    
+    with zipfile.ZipFile(filename, 'r') as zip_ref:
+        zip_ref.extractall("./report")
+
+    rpath = os.path.join(path, 'report')
+    if os.path.exists(rpath):
+        shutil.rmtree(rpath)
+
+    shutil.move('./report', path)
+    os.remove(filename)
+
+    pdf_link = f"{link}/output/output.pdf"
+    response = requests.get(pdf_link, cookies=cookies, stream=True)
+    response.raise_for_status()
+    
+    pdf_path = os.path.join(path, 'report', 'report.pdf')
+    with open(pdf_path, 'wb') as pdf_file:
+        for chunk in response.iter_content(chunk_size=8192):
+            pdf_file.write(chunk)
+        
+
+def fetch_cookies(browser: str):
+    fetchers = {
+        "firefox": browser_cookie3.firefox,
+        "chrome": browser_cookie3.chrome,
+        "safari": browser_cookie3.safari
+    }
+    cs = fetchers[browser](domain_name='overleaf.com')
+
+    return {c.name: c.value for c in cs}
 
 
+def parse_cmd():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tag', required=True, help="Session tag [check conf.yaml]")
 
-# Функция для замены переменных окружения
-def replace_env_vars(value):
-    # Используем регулярное выражение для поиска переменных окружения
-    return re.sub(r'\${(.*?)}', lambda match: os.getenv(match.group(1), ''), value)
-
-# Загружаем настройки из файла conf.yaml
-with open('conf.yaml', 'r', encoding='utf-8') as file:
-    config = yaml.safe_load(file)
-
-# Применяем замену переменных окружения
-for session_name, session_data in config['sessions'].items():
-    session_data['path'] = replace_env_vars(session_data['path'])
-
-# Пример доступа к данным
-browser = config['browser']
-sessions = config['sessions']
-
-# Выводим настройки
-print(f"Используемый браузер: {browser}")
-
-for session_name, session_data in sessions.items():
-    print(f"Сессия: {session_name}")
-    print(f"  Тег: {session_data['tag']}")
-    print(f"  Ссылка: {session_data['link']}")
-    print(f"  Путь: {session_data['path']}")
+    return parser.parse_args()
 
 
-# Получаем куки из Firefox для конкретного домена
-cookies = browser_cookie3.firefox(domain_name='overleaf.com')
+def main():
+    args = parse_cmd()
+    tag = args.tag
 
-# Преобразуем куки в словарь для использования в запросах
-cookies_dict = {cookie.name: cookie.value for cookie in cookies}
-print(cookies_dict)
+    with open('conf.yaml', 'r', encoding='utf-8') as file:
+        cfg = yaml.safe_load(file)
 
-download_zip("https://www.overleaf.com/project/672366509c5311ddb4355f7b", cookies)
+    browser = cfg['browser']
+    sessions = cfg['sessions']
+
+    link = None
+    path = None
+    for name, data in sessions.items():
+        if name == tag:
+            link = data['link']
+            path = data['path']
+            break
+    if link == None or path == None:
+        print('Broken conf.yaml')
+        exit(1)
+
+    cookies = fetch_cookies(browser)
+    process_report(link, path, cookies)
+
+if __name__ == "__main__":    
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    main()
